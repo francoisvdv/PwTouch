3c20bd9770c11962934e3a642e67f4a837525f03
Francois van der Ven <francoisvdv@freeler.nl>
2010-07-05 18:12:46 +0200

    MultiTouchVista toegevoegd.

---
+++ PwTouchInputProvider/InputProvider.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Windows;
+using Multitouch.Contracts;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace PwTouch
+{
+	[AddIn("PwTouch", Publisher = "Francois van der Ven", Description = "Provides input from PwTouch table.", Version = VERSION)]
+	[Export(typeof(IProvider))]
+	public class InputProvider : IProvider
+	{
+		internal const string VERSION = "1.0.0.0";
+        List<Contact> contacts = new List<Contact>();
+        System.Timers.Timer timer;
+
+        public InputProvider()
+        {
+            timer = new System.Timers.Timer(1000 / 60d);
+			timer.Elapsed += timer_Elapsed;
+
+            contacts.Add(new Contact(0, ContactState.New, new System.Windows.Point(10, 10), 1680, 1050));
+        }
+
+		public void Start()
+		{
+			timer.Start();
+		}
+
+		public void Stop()
+		{
+			timer.Stop();
+		}
+
+        public bool IsRunning
+        {
+            get { return true; }
+        }
+
+        public bool HasConfiguration
+        {
+            get { return true; }
+        }
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			EventHandler<NewFrameEventArgs> eventHandler = NewFrame;
+			if (eventHandler != null)
+				eventHandler(this, new NewFrameEventArgs(Stopwatch.GetTimestamp(), contacts, null));
+
+            contacts.Clear();
+            contacts.Add(new Contact(0, ContactState.Moved, new System.Windows.Point(10, 10), SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height));
+		}
+
+        public System.Windows.UIElement GetConfiguration()
+        {
+            return null;
+        }
+
+        public bool SendImageType(ImageType imageType, bool value)
+        {
+            return false;
+        }
+
+        public bool SendEmptyFrames
+        {
+            get
+            {
+                return true;
+            }
+            set
+            {
+
+            }
+        }
+
+        public event EventHandler<NewFrameEventArgs> NewFrame;
+    }
+}
---
+++ PwTouchInputProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PwTouchInputProvider")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Francois van der Ven")]
+[assembly: AssemblyProduct("PwTouch Input Provider")]
+[assembly: AssemblyCopyright("Copyright © Francois van der Ven 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("471d8821-a43d-4f07-8c07-6cb36b512f47")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
